39f7b990702c68b814786b29972a33513cf57a2f
Nasyarobby <nasyarobby@gmail.com>
2019-02-05 19:10:18 -0500

    added Transacation interface

--- index.d.ts
+++ index.d.ts
@@ -7,3 +7,11 @@ export interface IdnBankParser {
     login: Function;
     logout: Function;
 }
+
+export interface Transaction {
+    transactionDate: string|boolean; //YYYY/MM/DD or false if nothing
+    bookDate: string|boolean; //YYYY/MM/DD or false if nothing
+    entryType: string; // CR or DB
+    amount: number; // the amount of transactions
+    notes: string; // notes from the bank system, usually contain amount, description, other party's account, or reference number
+}
